587160f924eb720ed897ca86f7b7106022d06e80
Mario SHAYA <h2beatz13@gmail.com>
2025-11-02 20:48:38 +0000

    added config for neon blockchain

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+export const NATIVE_CURRENCY = "0x0000000000000000000000000000000000000000"
+
+export const config = {
+	chainId: 245022934,
+	name: "Neon EVM Mainnet",
+	rpc: "https://neon-mainnet.everstake.one",
+	scanExplorer: "https://neon.blockscout.com/tx/",
+}
