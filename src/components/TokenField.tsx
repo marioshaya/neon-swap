a70267fa6060904dedefb6af9274dda2d558b83f
Mario SHAYA <h2beatz13@gmail.com>
2025-11-04 07:12:06 +0000

    removed value attribute in token field input as it were only for debugs

--- src/components/TokenField.tsx
+++ src/components/TokenField.tsx
@@ -79,7 +79,6 @@ const TokenField = ({
 				type="text"
 				inputMode="decimal"
 				pattern="^\\d*\\.?\\d*$"
-				// value={amount}
 				value={isOutput ? (loading ? "..." : amount) : amount}
 				onInput={handleInput}
 				placeholder="0.00"
